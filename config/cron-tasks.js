ade23795ff51e4a202d705704a567f8e970436b7
huydq <wepiohip@gmail.com>
2024-01-03 15:40:53 +0700

    fix: crontask

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -1,10 +1,19 @@
 module.exports = {
   postCountEmail: {
     task: async ({ strapi }) => {
-      console.log(
-        `Sending email with published article count (run by Strapi cron)`
-      );
+      console.log(`--------- Bắt đầu chạy đếm Tracking ---------`);
 
+      const count = await strapi.db.query("api::response.response").count({
+        where: {
+          path: "/api/v1/users/me",
+          method: "GET",
+          status_code: 200,
+        },
+      });
+
+      console.log(count);
+
+      console.log(`=========== Kết thúc chạy đếm Tracking ==========`);
     },
     options: {
       // Every minute
