05a7f10a3ccbeea2641de22845ac32730b86c754
huydq <wepiohip@gmail.com>
2024-01-05 15:22:40 +0700

    refactor: cronjob

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -3,19 +3,19 @@ module.exports = {
     task: async ({ strapi }) => {
       console.log(`--------- Bắt đầu chạy đếm Tracking ---------`);
 
-      const count = await strapi.db.connection
-        .from("responses")
-        .orderBy("createdAt", "desc")
-        .where({
-          path: "/api/v1/users/me",
-          method: "GET",
-          status_code: 200,
-        })
-        .select("user_id")
-        .groupBy("user_id")
-        .count();
+      // const count = await strapi.db.connection
+      //   .from("responses")
+      //   .orderBy("createdAt", "desc")
+      //   .where({
+      //     path: "/api/v1/users/me",
+      //     method: "GET",
+      //     status_code: 200,
+      //   })
+      //   .select("user_id")
+      //   .groupBy("user_id")
+      //   .count();
 
-      console.log(count);
+      // console.log(count);
 
       console.log(`=========== Kết thúc chạy đếm Tracking ==========`);
     },
